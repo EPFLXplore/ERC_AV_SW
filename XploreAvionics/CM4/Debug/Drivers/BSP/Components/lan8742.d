67cb42696f22c46790ad744f7687a90ee87b1f33
Myriam <myriam.rihani00@gmail.com>
2020-11-11 11:51:27 +0100

    Barometre init

--- XploreAvionics/CM4/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- /Users/arion/git/XploreAvionics/XploreAvionics/Drivers/BSP/Components/lan8742/lan8742.c \
- /Users/arion/git/XploreAvionics/XploreAvionics/Drivers/BSP/Components/lan8742/lan8742.h
-
-/Users/arion/git/XploreAvionics/XploreAvionics/Drivers/BSP/Components/lan8742/lan8742.h:
