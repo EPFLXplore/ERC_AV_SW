1bafb718eddc34476c047122092e64994191e725
Yassine Bakkali <ybakkali8@gmail.com>
2022-08-28 02:26:40 +0200

    Added guided comments for what to do next and telemetry settings for science/HD/NAV boards

--- System/Telemetry.cpp
+++ System/Telemetry.cpp
@@ -32,6 +32,12 @@
 #elif defined(BUILD_FOR_SCIENCE_A)
 	STMUARTDriver UART1_driver(&huart1);
 	NetworkBus network(&UART1_driver);
+#elif defined(BUILD_FOR_SCIENCE_B)
+	STMUARTDriver UART6_driver(&huart6);
+	NetworkBus network(&UART6_driver);
+#elif defined(BUILD_FOR_HANDLING_DEVICE)
+	STMUARTDriver UART3_driver(&huart3);
+	NetworkBus network(&UART3_driver);
 #endif
 
 
@@ -63,28 +69,44 @@ void setupTelemetry() {
 	STMUARTDriver_list.push_back(&UART1_driver);
 	STMUARTDriver_list.push_back(&UART3_driver);
 	STMUARTDriver_list.push_back(&UART6_driver);
+
+	// DO BNO AND LIMIT SWITCHES
+
+	// SCIENCE -> AV
 	network.forward<sc_LED_packet>(&UART1_network);
 	network.forward<sc_trap_packet>(&UART1_network);
+	// network.forward<sc_hx711_tare_packet>(&UART1_network); //IMPORTANT
+	// network.forward<sc_hx711_calibrate_packet>(&UART1_network);	 //IMPORTANT
+	// AV -> SCIENCE
 	UART1_network.forward<sc_trap_success_packet>(&network);
 	UART1_network.forward<sc_caching_success_packet>(&network);
 	UART1_network.forward<avionics_voltmeter_packet>(&network);
+	UART1_network.forward<avionics_massload_packet>(&network);
+	UART1_network.forward<avionics_moisture_packet>(&network);
+
+	// HANDLING DEVICE -> AV
+	// network.forward<>(&UART3_network); // tare voltmeter
+	// network.forward<hd_voltmeter_motor>(&UART3_network);
+	// AV -> HANDLING DEVICE
+	UART3_network.forward<avionics_voltmeter_packet>(&network);
+	UART3_network.forward<avionics_TOF_packet>(&network);
+	// UART3_network.forward<avionics_current_packet>(&network);
 #elif defined(BUILD_FOR_SCIENCE_A)
 	STMUARTDriver_list.push_back(&UART1_driver);
 	network.handle<sc_LED_packet>(&handle_led);
 	network.handle<sc_trap_packet>(&handle_servo_trap);
 	network.handle<sc_caching_packet>(&handle_servo_caching);
-#endif
-//	network.forward<PingPacket>(&network);
-//	network.forward<sc_LED_packet>(&UART1_network);
-//	static STMUARTDriver UART1_driver(&huart1);
-//	static NetworkBus UART1_network(&UART1_driver);
-//
-//	static STMUARTDriver UART6_driver(&huart6);
-//	static NetworkBus UART6_network(&UART6_driver);
+#elif defined(BUILD_FOR_SCIENCE_B)
+	STMUARTDriver_list.push_back(&UART6_driver);
+	//network.handle<sc_hx711_tare_packet(&UART6_driver)>; //IMPORTANT
+	//network.handle<sc_hx711_calibrate_packet(&UART6_driver)>;
+#elif defined(BUILD_FOR_HANDLING_DEVICE)
+	STUMUARTDriver_list.push_back(&UART3_driver);
+	//network.handle<hd_tare_packet>(&handle_voltmeter_tare);
+	//network.handle<hd_motor_packet>(&handle_voltmeter_motor); //IMPORTANT
+	//network.handle<avionics_current_packet>(&handle_current)
 
-//	network.handle<sc_LED_packet>(&handle_led);
-////	network.forward<sc_LED_packet>(&UART6_network);
-//	network.forward<sc_LED_packet>(&UART1_network);
+#endif
 
 }
 
