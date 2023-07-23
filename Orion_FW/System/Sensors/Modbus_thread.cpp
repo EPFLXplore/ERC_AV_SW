959ac18927382a001d1521fe8c977a57a948973f
Vincent <vincen7.nguyen@gmail.com>
2023-07-23 23:45:55 +0200

    changed pin names

--- Orion_FW/System/Sensors/Modbus_thread.cpp
+++ Orion_FW/System/Sensors/Modbus_thread.cpp
@@ -41,10 +41,10 @@ void ModbusThread::init_Modbus(modbusHandler_t* ModbusH){
 	  ModbusH->port =  &huart2;
 	  ModbusH->u8id = 0; // For master it must be 0
 	  ModbusH->u16timeOut = 1000;
-	  ModbusH->EN_Port1 = GPIOD;
-	  ModbusH->EN_Pin1 = GPIO_PIN_4;
-	  ModbusH->EN_Port2 = GPIOD;
-	  ModbusH->EN_Pin2 = GPIO_PIN_14;
+	  ModbusH->EN_Port1 = HAT2_P5_GPIO_Port;
+	  ModbusH->EN_Pin1 = HAT2_P5_Pin;
+	  ModbusH->EN_Port2 = HAT2_P3_GPIO_Port;
+	  ModbusH->EN_Pin2 = HAT2_P3_Pin;
 	  ModbusH->u16regs = ModbusDATA;
 	  ModbusH->u16regsize= sizeof(ModbusDATA)/sizeof(ModbusDATA[0]);
 	  ModbusH->xTypeHW = USART_HW;
